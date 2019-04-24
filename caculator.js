7161f612aba1875377c07d7f62bb7340dac8136d
huongdp051092 <huongdp051092@gmail.com>
2019-04-24 15:44:53 +0700

    toi uu 4 ham ve 1 ham

--- caculator.js
+++ caculator.js
@@ -1,7 +1,7 @@
-document.getElementById("addition").onclick = function () {
+/*document.getElementById("addition").onclick = function () {
     let number1 = document.getElementById("number1").value;
     let number2 = document.getElementById("number2").value;
-    document.getElementById("result").innerHTML = "Result Division: " + (Number(number1) + Number(number2));
+    document.getElementById("result").innerHTML = "Result Addition: " + (Number(number1) + Number(number2));
 }
 document.getElementById("subtraction").onclick = function () {
     let number1 = document.getElementById("number1").value;
@@ -17,4 +17,21 @@ document.getElementById("division").onclick = function () {
     let number1 = document.getElementById("number1").value;
     let number2 = document.getElementById("number2").value;
     document.getElementById("result").innerHTML = "Result Division: " + (Number(number1) / Number(number2));
+}
+ */
+function caculator(value) {
+    let number1 = document.getElementById("number1").value;
+    let number2 = document.getElementById("number2").value;
+    if (value=="addition") {
+        document.getElementById("result").innerHTML = "Result Addition: " + (Number(number1) + Number(number2));
+    }
+    if (value=="subtraction") {
+        document.getElementById("result").innerHTML = "Result Subtraction: " + (Number(number1) - Number(number2));
+    }
+    if (value=="multiplication") {
+        document.getElementById("result").innerHTML = "Result Multiplication: " + (Number(number1) * Number(number2));
+    }
+    if (value=="division") {
+        document.getElementById("result").innerHTML = "Result Division: " + (Number(number1) / Number(number2));
+    }
 }
